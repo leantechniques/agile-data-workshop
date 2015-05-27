36ff3add854e29c5fbbcbc662c1d2a90aff0a984
Tim Andersen <tandersen@leantechniques.co>
2015-05-26 23:43:52 -0500

    cleaned up example for DbUnit

--- src/test/java/co/leantechniques/DbUnitExample.java
+++ src/test/java/co/leantechniques/DbUnitExample.java
@@ -1,10 +1,8 @@
 package co.leantechniques;
 
 import org.dbunit.Assertion;
-import org.dbunit.DatabaseTestCase;
 import org.dbunit.IDatabaseTester;
 import org.dbunit.JdbcDatabaseTester;
-import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.ITable;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
@@ -14,7 +12,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -24,54 +21,21 @@ import java.io.File;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:/test-contexts/test-context.xml")
-public class DbUnitExample extends DatabaseTestCase {
+public class DbUnitExample {
 
     @Autowired
     DataSource dataSource;
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-
     @Before
     public void importDataSet() throws Exception {
-        IDataSet dataSet = getDataSet();
-        cleanlyInsertDataset(dataSet);
-    }
-
-    private void cleanlyInsertDataset(IDataSet dataSet) throws Exception {
-        IDatabaseTester databaseTester = new JdbcDatabaseTester(
-                "com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/customer_dev?autoreconnect=true", "agiledba", "p@ssw0rd");
-        databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
-        databaseTester.setDataSet(dataSet);
-        databaseTester.onSetup();
-    }
-
-    @Override
-    protected IDatabaseConnection getConnection() throws Exception {
-        return new MySqlConnection(dataSource.getConnection(), null);
-    }
-
-    @Override
-    protected IDataSet getDataSet() throws Exception {
-        String resourceName = "dbunit/expected_people.xml";
-        File expectedDataSetXml = new File(getClass().getClassLoader().getResource(resourceName).toURI());
-        return new FlatXmlDataSetBuilder().build(expectedDataSetXml);
-    }
-
-    @Override
-    protected DatabaseOperation getSetUpOperation() throws Exception {
-        return DatabaseOperation.CLEAN_INSERT;
-    }
-
-    @Override
-    protected DatabaseOperation getTearDownOperation() throws Exception {
-        return DatabaseOperation.NONE;
+        cleanInsertDataFrom("dbunit/expected_people.xml");
     }
 
     @Test
     public void expectedDataMatchesTableContent() throws Exception {
         // Fetch database data after executing your code
-        IDataSet databaseDataSet = getConnection().createDataSet();
+        MySqlConnection connection = new MySqlConnection(dataSource.getConnection(), null);
+        IDataSet databaseDataSet = connection.createDataSet();
         ITable actualTable = databaseDataSet.getTable("people");
 
         // Load expected data from an XML dataset
@@ -82,6 +46,15 @@ public class DbUnitExample extends DatabaseTestCase {
 
         // Assert actual database table match expected table
         Assertion.assertEquals(expectedTable, actualTable);
+    }
+
+    private void cleanInsertDataFrom(String dbunitXmlFile) throws Exception {
+        File expectedDataSetXml = new File(getClass().getClassLoader().getResource(dbunitXmlFile).toURI());
+        IDataSet dataSet = new FlatXmlDataSetBuilder().build(expectedDataSetXml);
+        IDatabaseTester databaseTester = new JdbcDatabaseTester("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/customer_dev?autoreconnect=true", "agiledba", "p@ssw0rd");
+        databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+        databaseTester.setDataSet(dataSet);
+        databaseTester.onSetup();
     }
 
 }
